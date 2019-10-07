e911890ffbccf5b82c44bb13f0b6d807d4c0fc0f
Qiang Hao <qiang.hao@wwu.edu>
2019-10-07 12:56:23 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# cs145-lab2
+
+My Name: Please edit this.
+
+About This Project: Please edit this.
---
+++ src/main/java/NetPay.java
@@ -0,0 +1,32 @@
+import java.util.*;
+
+// this is the main class
+public class NetPay {
+	// // you need to uncomment the constants 
+	// // and declare the values of various deduction types
+	// private static final double FEDERAL_TAX_PERCENT = ;
+	// private static final double STATE_TAX_PERCENT = ;
+	// private static final double SS_PERCENT = ;
+	// private static final double MEDICARE_PERCENT = ;
+	// private static final double PAY_PER_HOUR = ;
+
+    public static void main(String[] args) {
+
+    	// an example of testing your code
+    	int numberOfHours = 40;
+    	double netPayValue = netPay(numberOfHours);
+
+    	// another example of testing your code
+    	numberOfHours = 30;
+    	netPayValue = netPay(numberOfHours);
+
+    	// you will eventually upgrade your main method to take inputs from users
+
+    }
+
+    // you need to comment on what this method does
+    public static double netPay(int numberOfHours) {
+    	// need to be deleted
+    	return 0.0;
+    }
+}
