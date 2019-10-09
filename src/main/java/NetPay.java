6f019331dee0d01c42870eb73516da348d81d82b
kaitlyn-rice <55768803+kaitlyn-rice@users.noreply.github.com>
2019-10-08 18:15:44 -0700

    Update NetPay.java
    part I: no user input

--- src/main/java/NetPay.java
+++ src/main/java/NetPay.java
@@ -4,12 +4,12 @@ import java.util.*;
 public class NetPay {
 	// // you need to uncomment the constants 
 	// // and declare the values of various deduction types
-	// private static final double FEDERAL_TAX_PERCENT = ;
-	// private static final double STATE_TAX_PERCENT = ;
-	// private static final double SS_PERCENT = ;
-	// private static final double MEDICARE_PERCENT = ;
-	// private static final double PAY_PER_HOUR = ;
-
+	 private static final double FEDERAL_TAX_PERCENT = 10.00;
+	 private static final double STATE_TAX_PERCENT = 4.50;
+	 private static final double SS_PERCENT = 6.20;
+	 private static final double MEDICARE_PERCENT = 1.45;
+	 private static final double PAY_PER_HOUR = 7.25;
+    
     public static void main(String[] args) {
 
     	// an example of testing your code
@@ -26,7 +26,32 @@ public class NetPay {
 
     // you need to comment on what this method does
     public static double netPay(int numberOfHours) {
-    	// need to be deleted
-    	return 0.0;
+      double grossPay;
+      double deductions;
+      double netPay= 0;     
+      System.out.println("Hours worked per week: " + numberOfHours);
+      grossPay = PAY_PER_HOUR * numberOfHours;
+      System.out.println("Gross pay: \t\t\t" + grossPay); 
+      System.out.println();
+      System.out.println("Deductions");
+      deductions = FEDERAL_TAX_PERCENT / 100 * grossPay;
+      deductions = Math.round(deductions*100.00)/100.00;
+      System.out.println("Federal: \t\t\t" + deductions);
+      netPay = grossPay - deductions;
+      deductions = STATE_TAX_PERCENT / 100 * grossPay;
+      deductions = Math.round(deductions*100.00)/100.00;
+      System.out.println("State: \t\t\t\t" + deductions);
+      netPay = netPay - deductions;
+      deductions = SS_PERCENT / 100 * grossPay;
+      deductions = Math.round(deductions*100.00)/100.00;
+      System.out.println("Social Security: \t" + deductions);
+      netPay = netPay - deductions;
+      deductions = MEDICARE_PERCENT / 100 * grossPay;
+      deductions = Math.round(deductions*100.00)/100.00;
+      System.out.println("Medicare: \t\t\t" + deductions);
+      netPay = netPay - deductions;
+      System.out.println();
+      System.out.println("Net Pay: \t\t\t" + netPay);
+      return netPay;
     }
 }
